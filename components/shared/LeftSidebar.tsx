fb2d02fbba267a1174b0575beeacc72de8de10ef
Jordan Tran <thai.jordan@gmail.com>
2023-08-08 15:26:05 -0700

    topbar and leftside bar in progress

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,3 +1,15 @@
+import { sidebarLinks } from "@/constants";
+
 export default function LeftSidebar() {
-    return <h1>Left side bar</h1>
+    return (
+        <section className="custom-scrollbar leftsidebar">
+            <div className="flex w-full flex-1 flex-col gap-6 px-6">
+                {sidebarLinks.map((link) => (
+                    <div>
+                        Link
+                    </div>
+                ))}
+            </div>
+        </section>
+    )
 }
