d0e71a8f39d9784188359bfa276d6fb419f3bedf
Jordan Tran <thai.jordan@gmail.com>
2023-08-09 14:18:48 -0700

    added shad for forms , user profile in progress.

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { useForm } from "react-hook-form";
+import { Form } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { UserValidation } from "@/lib/validations/user";
+
+interface Props {
+    user: {
+        id: string;
+        objectId: string;
+        username: string;
+        name: string;
+        bio: string;
+        image: string;
+    };
+    btnTitle: string;
+}
+
+export default function AccountProfile({ user, btnTitle }: Props) {
+    const form = useForm({
+        resolver: zodResolver(UserValidation),
+        defaultValues: {
+            profile_photo: '',
+            name: '',
+            username: '',
+            bio: '',
+        }
+    })
+    return (
+        <Form>
+
+        </Form>
+    )
+}
+
