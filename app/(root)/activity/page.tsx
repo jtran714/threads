4dfbfb66eadd6efaf7d0a397556b6fb6781d5164
Jordan Tran <thai.jordan@gmail.com>
2023-08-16 15:47:56 -0700

    finished usercard , activity in progress

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,4 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
+
 export default async function Page() {
+    const user = await currentUser();
+    if (!user) return null;
+  
+    const userInfo = await fetchUser(user.id);
+    if (!userInfo?.onboarded) redirect("/onboarding");
+  
+    const activity = await getActivity(userInfo._id);
+
     return (
         <section>
             user activity
