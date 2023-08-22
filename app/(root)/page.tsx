a3be4267c9ef94b73da63c3d9fb13a1ba13daa11
Jordan Tran <thai.jordan@gmail.com>
2023-08-21 17:47:19 -0700

    edited search page tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
-import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import ThreadCard from "@/components/cards/ThreadCard";
+import { currentUser } from "@clerk/nextjs";
+import UserCard from "@/components/cards/UserCard";
+import Searchbar from "@/components/shared/Searchbar";
 import Pagination from "@/components/shared/Pagination";
-import { fetchPosts } from "@/lib/actions/thread.actions";
-import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 
-export default async function Home({
+async function Page({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
@@ -16,42 +16,43 @@ export default async function Home({
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
 
-  const result = await fetchPosts(
-    searchParams.page ? +searchParams.page : 1,
-    30
-  );
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  });
 
   return (
-    <>
-      <h1 className='head-text text-left'>Home</h1>
+    <section>
+      <h1 className='head-text mb-10'>Search</h1>
 
-      <section className='mt-9 flex flex-col gap-10'>
-        {result.posts.length === 0 ? (
-          <p className='no-result'>No threads found</p>
+      <Searchbar routeType='search' />
+
+      <div className='mt-14 flex flex-col gap-9'>
+        {result.users.length === 0 ? (
+          <p className='no-result'>No Result</p>
         ) : (
           <>
-            {result.posts.map((post) => (
-              <ThreadCard
-                key={post._id}
-                id={post._id}
-                currentUserId={user.id}
-                parentId={post.parentId}
-                content={post.text}
-                author={post.author}
-                community={post.community}
-                createdAt={post.createdAt}
-                comments={post.children}
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType='User'
               />
             ))}
           </>
         )}
-      </section>
+      </div>
 
       <Pagination
-        path='/'
+        path='search'
         pageNumber={searchParams?.page ? +searchParams.page : 1}
         isNext={result.isNext}
       />
-    </>
+    </section>
   );
 }
